eefe7edf2cf81f74df3129ad940e63ddb2fe14a9
Lukasz Wisniewski <lwisniewski387@gmail.com>
2018-06-11 07:24:25 +0100

    #10 SSL On Localhost

---
+++ #10 SSL on Localhost/code/db.php
@@ -0,0 +1,17 @@
+<?php
+
+echo $abc;
+
+//$link = mysqli_connect("mysql", "adminn", "admin", "myproject");
+//
+//if (!$link) {
+//    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//    exit;
+//}
+//
+//echo "Success: A proper connection to MySQL was made!" . PHP_EOL;
+//echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+//
+//mysqli_close($link);
